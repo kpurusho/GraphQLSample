91d4ab226db48903a7f29195898fefe0fec8ceb3
Karthik Purushothaman <Karthik.Purushothaman@kla-tencor.com>
2020-10-31 10:37:59 +0530

    Initial version of sample

---
+++ App/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using GraphQL;
+using GraphQL.Types;
+using Json.Net;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string queryStr;
+            if (args.Count() == 1)
+            {
+                queryStr = args[0];
+            }
+            else
+            {
+                Console.WriteLine("Query string expected as argument");
+                return;
+            }
+
+            var schema = Schema.For(
+                File.ReadAllLines("schema.graphql"),
+                _ =>
+                {
+                    _.Types.Include<Query.Query>();
+                });
+
+            var execOptions = new ExecutionOptions
+            {
+                Schema = schema, 
+                Query = queryStr
+            };
+            var docExec = new DocumentExecuter();
+            var task = docExec.ExecuteAsync(execOptions);
+            task.Wait();
+            var result = JsonNet.Serialize(task.Result.Data);
+            Console.WriteLine($"Request: {queryStr}");
+            Console.WriteLine($"Response: {result}");
+        }
+    }
+}
---
+++ App/Query/LotQuery.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using App.Data;
+using App.Repository;
+using GraphQL;
+
+namespace App.Query
+{
+    public class Query
+    {
+        [GraphQLMetadata("lot")]
+        public Lot GetLot()
+        {
+            return LotDB.Lot;
+        }
+
+        [GraphQLMetadata("wafers")]
+        public IEnumerable<Wafer> GetWafers()
+        {
+            return LotDB.Lot.Wafers;
+        }
+
+        [GraphQLMetadata("wafer")]
+        public Wafer GetWafer(int id)
+        {
+            return LotDB.Lot.Wafers.FirstOrDefault(w => w.Id == id);
+        }
+
+        [GraphQLMetadata("defects")]
+        public IEnumerable<Defect> GetDefects(int waferid)
+        {
+            return LotDB.Lot.Wafers.FirstOrDefault(w => w.Id == waferid)?.Defects;
+        }
+
+        [GraphQLMetadata("defect")]
+        public Defect GetDefect(int waferid, int id)
+        {
+            return LotDB.Lot.Wafers.FirstOrDefault(w => w.Id == waferid)?.Defects.FirstOrDefault(d => d.Id == id);
+        }
+    }
+}
